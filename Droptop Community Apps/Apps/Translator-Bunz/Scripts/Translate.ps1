1aef14f3b73f0bdc3f78737de198f5dc2ce6801f
Bunz <66bunz@gmail.com>
2023-08-26 15:39:58 +0200

    4.321.31

--- Droptop Community Apps/Apps/Translator-Bunz/Scripts/Translate.ps1
+++ Droptop Community Apps/Apps/Translator-Bunz/Scripts/Translate.ps1
@@ -1,26 +1,18 @@
+
 function Translate {
     $SourceLanguage = $RmAPI.OptionStr('SourceLanguage')
-    # write-host $SourceLanguage
-
     $TargetLanguage = $RmAPI.OptionStr('TargetLanguage')
-    # write-host $TargetLanguage
-
     $Text = $RmAPI.OptionStr('Text')
-    # write-host $Text
-
     $VarsLocation = $RmAPI.OptionStr('VarsLocation')
-    # write-host $VarsLocation
 
-    $Uri = “https://translate.googleapis.com/translate_a/single?client=gtx&sl=$($SourceLanguage)&tl=$($TargetLanguage)&dt=t&q=$Text”
+    $Uri = “https://translate.googleapis.com/translate_a/single?client=gtx&sl=$($SourceLanguage)&tl=$($TargetLanguage)&dt=t&q=$($Text)”
 
     $Response = Invoke-RestMethod -Uri $Uri -Method Get
-
     $Translation = $Response[0].SyncRoot | ForEach-Object { $_[0] }
-
-    # write-host $Translation
 
     $RmAPI.Bang('!SetVariable Translation1 "'+$Translation+'"')
     $RmAPI.Bang('!WriteKeyValue Variables Translation1 "'+$Translation+'" "'+$VarsLocation+'"')
-    
     $RmAPI.Bang('!SetVariable CopyActive 1')
+    $RmAPI.Bang('!UpdateMeter *')
+    $RmAPI.Bang('!Update')
 }
