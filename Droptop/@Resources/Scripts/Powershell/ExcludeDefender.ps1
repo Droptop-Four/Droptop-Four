d9aa93597034db4db789c6adc3d3942e2de2f53b
Cariboudjan <75376803+Cariboudjan@users.noreply.github.com>
2023-11-22 16:16:25 -0700

    Update ExcludeDefender.ps1

--- Droptop/@Resources/Scripts/Powershell/ExcludeDefender.ps1
+++ Droptop/@Resources/Scripts/Powershell/ExcludeDefender.ps1
@@ -1,20 +1,14 @@
-$skinspath=$args[0]
-$programpath=$args[1]
-
-$folderPath = "$skinspath"
-
 $isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-	
+
 if (-not $isAdmin) {
 	$startInfo = New-Object System.Diagnostics.ProcessStartInfo
 	$startInfo.FileName = 'powershell.exe'
-	$startInfo.Arguments = "-Command `"Add-MpPreference -ExclusionPath '$folderPath'`""
+	$startInfo.Arguments = "-Command `"Add-MpPreference -ExclusionPath '$skinsPath'`""
 	$startInfo.Verb = "runas"
 [System.Diagnostics.Process]::Start($startInfo) | Out-Null
 } else {
-	Add-MpPreference -ExclusionPath $folderPath
 	Set-ExecutionPolicy RemoteSigned -Force
-	
-	powercfg /setdcvalueindex SCHEME_CURRENT SUB_ENERGYSAVER ESBATTTHRESHOLD 20
-	powercfg /setactive scheme_current
 }
+
+powercfg /setdcvalueindex SCHEME_CURRENT SUB_ENERGYSAVER ESBATTTHRESHOLD 20
+powercfg /setactive scheme_current
