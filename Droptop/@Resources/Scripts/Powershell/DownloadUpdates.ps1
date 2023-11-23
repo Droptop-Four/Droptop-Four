1b4cd61f973df25ba24e297fcb00f3415522ac9e
Cariboudjan <75376803+Cariboudjan@users.noreply.github.com>
2023-11-23 14:56:35 -0700

    Update DownloadUpdates.ps1

--- Droptop/@Resources/Scripts/Powershell/DownloadUpdates.ps1
+++ Droptop/@Resources/Scripts/Powershell/DownloadUpdates.ps1
@@ -28,25 +28,25 @@ Invoke-WebRequest -Uri "https://github.com/Droptop-Four/Droptop-Four/releases/la
 
 # Write-Host "Downloading latest version of $appname1. Please wait..."
 
-Invoke-WebRequest -Uri '$appURL1' -OutFile ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName1.rmskin"
+Invoke-WebRequest -Uri "$appURL1" -OutFile ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName1.rmskin"
 
 # Write-Host "Downloading latest version of $appname2. Please wait..."
 	
-Invoke-WebRequest -Uri '$appURL2' -OutFile ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName2.rmskin"
+Invoke-WebRequest -Uri "$appURL2" -OutFile ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName2.rmskin"
 
 
 # Write-Host "Downloading latest version of $appname3. Please wait..."
 	
-Invoke-WebRequest -Uri '$appURL3' -OutFile ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName3.rmskin"
+Invoke-WebRequest -Uri "$appURL3" -OutFile ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName3.rmskin"
 
 # Write-Host "Downloading latest version of $appname4. Please wait..."
 	
-Invoke-WebRequest -Uri '$appURL4' -OutFile ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName4.rmskin"
+Invoke-WebRequest -Uri "$appURL4" -OutFile ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName4.rmskin"
 
 
 # Write-Host "Downloading latest version of $appname5. Please wait..."
 	
-Invoke-WebRequest -Uri '$appURL5' -OutFile ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName5.rmskin"
+Invoke-WebRequest -Uri "$appURL5" -OutFile ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName5.rmskin"
 
 
 
