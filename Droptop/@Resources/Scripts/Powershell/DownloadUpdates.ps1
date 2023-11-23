c5bf8a88f1f9f9e4c0f0c0d16c28dc97649f07b6
Cariboudjan <75376803+Cariboudjan@users.noreply.github.com>
2023-11-23 14:20:27 -0700

    Update DownloadUpdates.ps1

--- Droptop/@Resources/Scripts/Powershell/DownloadUpdates.ps1
+++ Droptop/@Resources/Scripts/Powershell/DownloadUpdates.ps1
@@ -17,57 +17,12 @@ if (-not $isAdmin) {
 	Set-ExecutionPolicy RemoteSigned -Force
 }
 
-Write-Host ""
-Write-Host "Droptop update in progress..."
+# Write-Host "Droptop update in progress..."
 
 New-Item -ItemType Directory -Path ".\Droptop Folders\Other files\@Downloads\Droptop Updates"
 New-Item -ItemType Directory -Path ".\Droptop Folders\Other files\@Downloads\Community App Updates"
 
-if ($downloadMode -eq 1)
-{
-	if ( $updateAvailable1 -eq 1 )
-	{
-		Write-Host ""
-		Write-Host "Downloading latest version of $appname1. Please wait..."
-
-		Invoke-WebRequest -Uri "$appURL1" -OutFile ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName1.rmskin"
-	}
-
-	if ( $updateAvailable2 -eq 1 )
-	{
-		Write-Host ""
-		Write-Host "Downloading latest version of $appname2. Please wait..."
-		
-		Invoke-WebRequest -Uri "$appURL2" -OutFile ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName2.rmskin"
-	}
-
-	if ( $updateAvailable3 -eq 1 )
-	{
-		Write-Host ""
-		Write-Host "Downloading latest version of $appname3. Please wait..."
-		
-		Invoke-WebRequest -Uri "$appURL3" -OutFile ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName3.rmskin"
-	}
-
-	if ( $updateAvailable4 -eq 1 )
-	{
-		Write-Host ""
-		Write-Host "Downloading latest version of $appname4. Please wait..."
-		
-		Invoke-WebRequest -Uri "$appURL4" -OutFile ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName4.rmskin"
-	}
-
-	if ( $updateAvailable5 -eq 1 )
-	{
-		Write-Host ""
-		Write-Host "Downloading latest version of $appname5. Please wait..."
-		
-		Invoke-WebRequest -Uri "$appURL5" -OutFile ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName5.rmskin"
-	}
-}
-
-Write-Host ""
-Write-Host "Downloading Droptop update $newVersion. Please wait..."
+# Write-Host "Downloading Droptop update $newVersion. Please wait..."
 
 Invoke-WebRequest -Uri "https://github.com/Droptop-Four/Droptop-Four/releases/latest/download/Droptop_Update.rmskin" -OutFile ".\Droptop Folders\Other files\@Downloads\Droptop Updates\Droptop Update $downloadDate.rmskin"
 
@@ -75,49 +30,84 @@ if ($downloadMode -eq 1)
 {
 	if ( $updateAvailable1 -eq 1 )
 	{
-		Write-Host ""
-		Write-Host "Installing latest version of $appname1. Please wait..."
+		# Write-Host "Downloading latest version of $appname1. Please wait..."
+
+		Invoke-WebRequest -Uri "$appURL1" -OutFile ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName1.rmskin"
+	}
+
+	if ( $updateAvailable2 -eq 1 )
+	{
+		# Write-Host "Downloading latest version of $appname2. Please wait..."
+		
+		Invoke-WebRequest -Uri "$appURL2" -OutFile ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName2.rmskin"
+	}
+
+	if ( $updateAvailable3 -eq 1 )
+	{
+		# Write-Host "Downloading latest version of $appname3. Please wait..."
+		
+		Invoke-WebRequest -Uri "$appURL3" -OutFile ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName3.rmskin"
+	}
+
+	if ( $updateAvailable4 -eq 1 )
+	{
+		# Write-Host "Downloading latest version of $appname4. Please wait..."
+		
+		Invoke-WebRequest -Uri "$appURL4" -OutFile ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName4.rmskin"
+	}
+
+	if ( $updateAvailable5 -eq 1 )
+	{
+		# Write-Host "Downloading latest version of $appname5. Please wait..."
+		
+		Invoke-WebRequest -Uri "$appURL5" -OutFile ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName5.rmskin"
+	}
+}
+
+if ($downloadMode -eq 1)
+{
+	if ( $updateAvailable1 -eq 1 )
+	{
+		# Write-Host "Installing latest version of $appname1. Please wait..."
 		
 		& ".\Droptop\@Resources\Scripts\AppBuilder\RMSKINInstaller.exe" --norestart --skin ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName1.rmskin"
 	}
 
 	if ( $updateAvailable2 -eq 1 )
 	{
-		Write-Host ""
-		Write-Host "Installing latest version of $appname2. Please wait..."
+		# Write-Host "Installing latest version of $appname2. Please wait..."
 		
 		& ".\Droptop\@Resources\Scripts\AppBuilder\RMSKINInstaller.exe" --norestart --skin ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName2.rmskin"
 	}
 
 	if ( $updateAvailable3 -eq 1 )
 	{
-		Write-Host ""
-		Write-Host "Installing latest version of $appname3. Please wait..."
+		# Write-Host "Installing latest version of $appname3. Please wait..."
 		
 		& ".\Droptop\@Resources\Scripts\AppBuilder\RMSKINInstaller.exe" --norestart --skin ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName3.rmskin"
 	}
 
 	if ( $updateAvailable4 -eq 1 )
 	{
-		Write-Host ""
-		Write-Host "Installing latest version of $appname4. Please wait..."
+		# Write-Host "Installing latest version of $appname4. Please wait..."
 		
 		& ".\Droptop\@Resources\Scripts\AppBuilder\RMSKINInstaller.exe" --norestart --skin ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName4.rmskin"
 	}
 
 	if ( $updateAvailable5 -eq 1 )
 	{
-		Write-Host ""
-		Write-Host "Installing latest version of $appname5. Please wait..."
+		# Write-Host "Installing latest version of $appname5. Please wait..."
 		
 		& ".\Droptop\@Resources\Scripts\AppBuilder\RMSKINInstaller.exe" --norestart --skin ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName5.rmskin"
 	}
+ 
+Start-Process -FilePath "$programPath" -ArgumentList "!CommandMeasure","RunCommand.Restart",'"Run"','"Droptop\Other\WindowMenu"'
+
 }
 
-Write-Host ""
-Write-Host "Installing Droptop update $newVersion. Please wait..."
-
+# Write-Host "Installing Droptop update $newVersion. Please wait..."
 
 # & ".\Droptop\@Resources\Scripts\AppBuilder\RMSKINInstaller.exe" --norestart --skin ".\Droptop Folders\Other files\@Downloads\Droptop Updates\Droptop Update $downloadDate.rmskin"
 
 Start-Process -FilePath "$programPath" -ArgumentList "!CommandMeasure","RunCommand.Restart",'"Run"','"Droptop\Other\WindowMenu"'
+
