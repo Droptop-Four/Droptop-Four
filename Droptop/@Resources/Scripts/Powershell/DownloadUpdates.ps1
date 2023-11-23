d0bf524cd076598c1cf59d09abc18e2e95a0e8f6
Cariboudjan <75376803+Cariboudjan@users.noreply.github.com>
2023-11-22 19:55:24 -0700

    Update DownloadUpdates.ps1

--- Droptop/@Resources/Scripts/Powershell/DownloadUpdates.ps1
+++ Droptop/@Resources/Scripts/Powershell/DownloadUpdates.ps1
@@ -29,7 +29,8 @@ if ($downloadMode -eq 1)
 	{
 		Write-Host ""
 		Write-Host "Downloading latest version of $appname1. Please wait..."
-		
+		Start-Process -FilePath "$programPath" -ArgumentList "!SetVariable","DownloadState","1","Droptop\DropdownBar"
+
 		Invoke-WebRequest -Uri "$appURL1" -OutFile ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName1.rmskin"
 	}
 
@@ -37,6 +38,7 @@ if ($downloadMode -eq 1)
 	{
 		Write-Host ""
 		Write-Host "Downloading latest version of $appname2. Please wait..."
+		Start-Process -FilePath "$programPath" -ArgumentList "!SetVariable","DownloadState","2","Droptop\DropdownBar"
 		
 		Invoke-WebRequest -Uri "$appURL2" -OutFile ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName2.rmskin"
 	}
@@ -45,6 +47,7 @@ if ($downloadMode -eq 1)
 	{
 		Write-Host ""
 		Write-Host "Downloading latest version of $appname3. Please wait..."
+		Start-Process -FilePath "$programPath" -ArgumentList "!SetVariable","DownloadState","3","Droptop\DropdownBar"
 		
 		Invoke-WebRequest -Uri "$appURL3" -OutFile ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName3.rmskin"
 	}
@@ -53,6 +56,7 @@ if ($downloadMode -eq 1)
 	{
 		Write-Host ""
 		Write-Host "Downloading latest version of $appname4. Please wait..."
+		Start-Process -FilePath "$programPath" -ArgumentList "!SetVariable","DownloadState","4","Droptop\DropdownBar"
 		
 		Invoke-WebRequest -Uri "$appURL4" -OutFile ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName4.rmskin"
 	}
@@ -61,6 +65,7 @@ if ($downloadMode -eq 1)
 	{
 		Write-Host ""
 		Write-Host "Downloading latest version of $appname5. Please wait..."
+		Start-Process -FilePath "$programPath" -ArgumentList "!SetVariable","DownloadState","5","Droptop\DropdownBar"
 		
 		Invoke-WebRequest -Uri "$appURL5" -OutFile ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName5.rmskin"
 	}
@@ -68,6 +73,7 @@ if ($downloadMode -eq 1)
 
 Write-Host ""
 Write-Host "Downloading Droptop update $newVersion. Please wait..."
+		Start-Process -FilePath "$programPath" -ArgumentList "!SetVariable","DownloadState","6","Droptop\DropdownBar"
 
 Invoke-WebRequest -Uri "https://github.com/Droptop-Four/Droptop-Four/releases/latest/download/Droptop_Update.rmskin" -OutFile ".\Droptop Folders\Other files\@Downloads\Droptop Updates\Droptop Update $downloadDate.rmskin"
 
@@ -77,6 +83,7 @@ if ($downloadMode -eq 1)
 	{
 		Write-Host ""
 		Write-Host "Installing latest version of $appname1. Please wait..."
+		Start-Process -FilePath "$programPath" -ArgumentList "!SetVariable","DownloadState","7","Droptop\DropdownBar"
 		
 		& ".\Droptop\@Resources\Scripts\AppBuilder\RMSKINInstaller.exe" --norestart --skin ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName1.rmskin"
 	}
@@ -85,6 +92,7 @@ if ($downloadMode -eq 1)
 	{
 		Write-Host ""
 		Write-Host "Installing latest version of $appname2. Please wait..."
+		Start-Process -FilePath "$programPath" -ArgumentList "!SetVariable","DownloadState","8","Droptop\DropdownBar"
 		
 		& ".\Droptop\@Resources\Scripts\AppBuilder\RMSKINInstaller.exe" --norestart --skin ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName2.rmskin"
 	}
@@ -93,6 +101,7 @@ if ($downloadMode -eq 1)
 	{
 		Write-Host ""
 		Write-Host "Installing latest version of $appname3. Please wait..."
+		Start-Process -FilePath "$programPath" -ArgumentList "!SetVariable","DownloadState","9","Droptop\DropdownBar"
 		
 		& ".\Droptop\@Resources\Scripts\AppBuilder\RMSKINInstaller.exe" --norestart --skin ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName3.rmskin"
 	}
@@ -101,6 +110,7 @@ if ($downloadMode -eq 1)
 	{
 		Write-Host ""
 		Write-Host "Installing latest version of $appname4. Please wait..."
+		Start-Process -FilePath "$programPath" -ArgumentList "!SetVariable","DownloadState","10","Droptop\DropdownBar"
 		
 		& ".\Droptop\@Resources\Scripts\AppBuilder\RMSKINInstaller.exe" --norestart --skin ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName4.rmskin"
 	}
@@ -109,6 +119,7 @@ if ($downloadMode -eq 1)
 	{
 		Write-Host ""
 		Write-Host "Installing latest version of $appname5. Please wait..."
+		Start-Process -FilePath "$programPath" -ArgumentList "!SetVariable","DownloadState","11","Droptop\DropdownBar"
 		
 		& ".\Droptop\@Resources\Scripts\AppBuilder\RMSKINInstaller.exe" --norestart --skin ".\Droptop Folders\Other files\@Downloads\Community App Updates\$appName5.rmskin"
 	}
@@ -116,5 +127,6 @@ if ($downloadMode -eq 1)
 
 Write-Host ""
 Write-Host "Installing Droptop update $newVersion. Please wait..."
+		Start-Process -FilePath "$programPath" -ArgumentList "!SetVariable","DownloadState","12","Droptop\DropdownBar"
 
 & ".\Droptop\@Resources\Scripts\AppBuilder\RMSKINInstaller.exe" --skin ".\Droptop Folders\Other files\@Downloads\Droptop Updates\Droptop Update $downloadDate.rmskin"
