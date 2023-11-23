ad041e7f9a12a82e77d6e6256ebb2664d4059529
Cariboudjan <75376803+Cariboudjan@users.noreply.github.com>
2023-11-23 09:23:53 -0700

    Update DownloadUpdates.ps1

--- Droptop/@Resources/Scripts/Powershell/DownloadUpdates.ps1
+++ Droptop/@Resources/Scripts/Powershell/DownloadUpdates.ps1
@@ -13,8 +13,7 @@ if (-not $isAdmin) {
 	$startInfo.Verb = "runas"
 [System.Diagnostics.Process]::Start($startInfo) | Out-Null
 } else {
-	# Add-MpPreference -ExclusionPath $folderPath
-	# Set-ExecutionPolicy RemoteSigned -Force
+	Start-Process -FilePath "$programPath" -ArgumentList "!Quit"
 }
 
 Write-Host ""
