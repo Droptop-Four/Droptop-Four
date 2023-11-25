7887899ace5a1a0309b567685dd15f86e0904bc6
Cariboudjan <75376803+Cariboudjan@users.noreply.github.com>
2023-11-25 01:19:46 -0700

    Update ReplaceInFile.ps1

--- Droptop/@Resources/Scripts/Powershell/ReplaceInFile.ps1
+++ Droptop/@Resources/Scripts/Powershell/ReplaceInFile.ps1
@@ -1,7 +1,7 @@
-$replace1=$args[0]
-$replace2=$args[1]
-$path=$args[2]
-$encoding=$args[3]
+# $replace1=$args[0]
+# $replace2=$args[1]
+# $path=$args[2]
+# $encoding=$args[3]
 
 (Get-Content $path) -replace $replace1,$replace2 | out-file $path
 Set-Content -NoNewline -Encoding OEM $path -Value (Get-Content -Raw -Encoding $encoding $path)
