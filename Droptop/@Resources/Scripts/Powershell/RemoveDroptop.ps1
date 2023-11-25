c107fa0b7c1d1c55b925b817d5bcce4d0ceb55fd
Cariboudjan <75376803+Cariboudjan@users.noreply.github.com>
2023-11-25 09:10:54 -0700

    Update RemoveDroptop.ps1

--- Droptop/@Resources/Scripts/Powershell/RemoveDroptop.ps1
+++ Droptop/@Resources/Scripts/Powershell/RemoveDroptop.ps1
@@ -7,16 +7,12 @@ if (-not $isAdmin) {
 	Start-Process -FilePath "$programPath" -ArgumentList "!DeactivateConfigGroup", "DroptopSuite"
 	Start-Process -FilePath "$programPath" -ArgumentList "!DeactivateConfig", "Droptop\DropdownBar\AppBar"
 	Start-Process -FilePath "$programPath" -ArgumentList "!Quit"
-	
-	cd "$skinsPath"
 
-	Remove-Item -Path ".\Droptop Community Apps" -Recurse
-	Remove-Item -Path ".\Droptop Folders" -Recurse
-	Remove-Item -Path ".\Droptop" -Recurse
+	Remove-Item -Path "$skinsPath\Droptop Community Apps" -Recurse
+	Remove-Item -Path "$skinsPath\Droptop Folders" -Recurse
+	Remove-Item -Path "$skinsPath\Droptop" -Recurse
 
-	cd "$appData"
-
-	Remove-Item -Path ".\Rainmeter\Layouts\Droptop" -Recurse
+	Remove-Item -Path "$appData\Rainmeter\Layouts\Droptop" -Recurse
 
 	Start-Process -FilePath "$programPath"
 	Exit
